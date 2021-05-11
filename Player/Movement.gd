fc5ff7d523bffcf851b5e80a0dc61c962155edda
Mathichai Dechdee <44646767+mdechdee@users.noreply.github.com>
2021-05-11 22:39:52 +0700

    Reverts Movement States, Fix bugs

--- Player/Movement.gd
+++ Player/Movement.gd
@@ -1,16 +1,54 @@
 extends Node
 class_name Movement
 
-var move: Vector2
+onready var body: KinematicBody2D = owner
+export(float, 50, 100) var MOVE_SPEED
+export(float, 2000, 3000) var DASH_SPEED
 
-func _physics_process(delta):
-	for child in get_children():
-		child = child as State
-		if child.is_on:
-			child.physics_process(delta)
-			move = child.move # for debugging
-			break
+var move: Vector2
+var is_dashing: bool = false
+
+func _ready():
+	owner.connect("on_move", self, "do_move")
+	owner.connect("on_dash", self, "do_dash")
 	
+func _physics_process(_delta):
+	if is_dashing:
+		on_dash()
+	else:
+		on_move()
+	if GlobalVar.DEBUG:
+		debug()
+
+func do_move(dir: Vector2):
+	if(is_dashing):
+		return
+	if(dir == Vector2.ZERO):
+		move.x = lerp(move.x,0,0.2)
+	move += dir * MOVE_SPEED
+	
+func do_dash(dir:Vector2):
+	if is_dashing:
+		return
+	move = dir * DASH_SPEED
+	is_dashing = true
+	$DashTimer.start()
+
+func on_move():
+	move.y += GlobalVar.GRAVITY
+	move.y = clamp(move.y, -GlobalVar.MAX_Y_SPEED, GlobalVar.MAX_Y_SPEED)
+	move.x = clamp(move.x, -GlobalVar.MAX_X_SPEED, GlobalVar.MAX_X_SPEED)
+	move = body.move_and_slide(move)
+	if move.length() < 0.01:
+		move = Vector2.ZERO
+		
+func on_dash():
+	move = lerp(move, Vector2.ZERO, 0.2)
+	move = body.move_and_slide(move)
+
 func debug():
 	$"../SpeedDebugger".text = "%.3f, %.3f" % [move.x,move.y]
 	$"../Name".text = owner.name
+
+func _on_DashTimer_timeout():
+	is_dashing = false
